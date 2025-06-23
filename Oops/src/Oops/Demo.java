835b658d812d5fc0accfbe6f0b5daa3dcc681f0d
yogeshwarbhardwaj <yogeshwar.bhardwaj_cs22@gla.ac.in>
2025-06-23 11:53:14 +0530

    [yogeshwar] add.demo

---
+++ Oops/src/Oops/Demo.java
@@ -0,0 +1,15 @@
+package Oops;
+
+public class Demo {
+	int noofstudents;
+	int nooftables;
+	public static void main (String[] args) {
+		Demo cg = new Demo();
+		cg.noofstudents = 100;
+		cg.nooftables = 10;
+		System.out.println(cg.noofstudents);
+		System.out.println(cg.nooftables);
+		
+	}
+
+}
