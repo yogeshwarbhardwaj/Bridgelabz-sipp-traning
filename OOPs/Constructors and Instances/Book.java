5ef48ea35c2c91f24c758ff2c68bbd3e41772d5b
yogeshwarbhardwaj <yogeshwar.bhardwaj_cs22@gla.ac.in>
2025-06-24 21:34:41 +0530

    [yogeshwar] add.book

---
+++ OOPs/Constructors and Instances/Book.java
@@ -0,0 +1,46 @@
+public class Book {
+    // Attributes
+    String title;
+    String author;
+    double price;
+
+    // Default constructor
+    public Book() {
+        // Initialize with default values
+        this.title = "";
+        this.author = "";
+        this.price = 0.0;
+    }
+
+    // Parameterized constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Optional: Getters and setters (if needed)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
